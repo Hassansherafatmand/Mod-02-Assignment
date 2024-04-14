474f14b07bcb33c36728518ce7bb9c6427d7655f
Hassan Sherafatmand <sherafatmand.hs@gmail.com>
2024-04-14 13:40:38 -0700

    I have done the Module 2 assignment

---
+++ index.js
@@ -0,0 +1,23 @@
+/*
+3 Examples of basic arithmetic in JS
+2 Examples of declaring variables in JS
+2 Examples of using variables in arithmetic operations in JS 
+*/
+
+console.log("Module 02 Assignment");
+
+console.log(15 % 4);
+console.log((3 * 4) + 20 / 2);
+console.log(3 * 8 - (5 * 3));
+
+var age = 14;
+var test = 20;
+
+let myScore = 20;
+let userName = "Hassan";
+
+/*********************************/
+let myGradeMath = 90;
+let myGradePhysic = 60;
+console.log("Your Grade is: "+(myGradeMath + myGradePhysic) / 2);
+console.log(myGradeMath + 20);
